63e0298e923b7acde90e1889304c3a39e94d2ddf
Pandorolo <euzin05@gmail.com>
2020-11-20 21:28:15 +0100

    Created basic reroll system

--- farkle.py
+++ farkle.py
@@ -26,31 +26,53 @@ def get_players():
 # Control the current hand
 def hand_controller(index):
 	# Print the active player and generate his hand
-    clear_screen()
+    # clear_screen()
     print(f"It's the turn of player n.{index}")
     player_hand = [random.randint(1, 6) for y in range(6)]
+    temp_score = 0
+    reroll = 1
 
-    # Print the Hand
-    print("Your hand is: ", end=" ")
-    for n in player_hand:
-        print(n, end=" ")
-    print("\n")
+    while reroll == 1:
+        # Print the Hand
+        print("Your hand is: ", end=" ")
+        for n in player_hand:
+            print(n, end=" ")
+        print("\n")
 
-    # Ask for the die/dice the player wants to keep
-    # and check if it's in the list
-    player_input = int(input("Insert the numbers you want to keep: "))
-    player_points = 0
-    while player_input not in player_hand:
-        print("Please insert a number that's in the list.")
+        # Ask for the die/dice the player wants to keep
+        # and check if it's in the list
         player_input = int(input("Insert the numbers you want to keep: "))
+        player_points = 0
+        while player_input not in player_hand:
+            print("Please insert a number that's in the list.")
+            player_input = int(input("Insert the numbers you want to keep: "))
 
-    # Count and add the points
-    if player_hand.count(player_input) >= 3:
-    	scores[index] += player_input*100
-    elif player_input == 1:
-    	scores[index] += 100
-    elif player_input == 5:
-    	scores[index] += 50
+        # Count and add the points
+        if player_hand.count(player_input) >= 3 and player_input != 1:
+        	temp_score += player_input*100
+        	player_hand.remove(player_input)
+        	
+        elif player_hand.count(player_input) >= 3 and player_input == 1:
+            temp_score += player_input*1000 
+            player_hand.remove(player_input)
+            
+        elif player_input == 1:
+        	temp_score += 100
+        	player_hand.remove(player_input)
+        	
+        elif player_input == 5:
+        	temp_score += 50
+        	player_hand.remove(player_input)
+
+        reroll = str(input("Reroll? [Y/n]"))
+
+        if reroll == 'Y' or reroll == 'y':
+            reroll = 1
+        else:
+            reroll = 0
+
+    score[index] += temp_score
+    print(score)
 
 # Control Turns
 def turn_controller():
